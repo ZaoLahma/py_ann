5246f804bd2c68db8d9b889cb14c0cf71ae6e85f
Janne Thyberg <thyberg.jan@gmail.com>
2016-07-04 13:56:59 +0000

    Started implementing neural network. Not working, obviously.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def sigmoid(x):
 def sigmoid_prim(x):
 	return (x * (1 - x))
 
-class Perceptron:
+class Neutron:
 	def __init__(self, num_weights):
 		self.weights = []
 		for i in range(0, num_weights):
@@ -39,10 +39,47 @@ class Perceptron:
 			index += 1
 		return result
 
+class Network:
+	def __init__(self, num_hidden_layers, num_inputs, num_outputs):
+		self.neutron_layers = []
+		#Input layer
+		neutrons = []		
+		for i in range(0, num_inputs):
+			neutrons.append(Neutron(num_inputs))
+		self.neutron_layers.append(neutrons)
+		#Hidden layers
+		for i in range(0, num_hidden_layers):
+			neutrons = []
+			for n in range(0, num_inputs):
+				neutrons.append(Neutron(num_inputs))
+			self.neutron_layers.append(neutrons)
+		#Output layer
+		neutrons = []
+		for i in range(0, num_outputs):
+			neutrons.append(Neutron(num_inputs))
+		self.neutron_layers.append(neutrons)
+		
+	def feed_forward(self, values):
+		layer_results = []
+		layer_result = []
+		for neutron in self.neutron_layers[0]:
+			layer_result.append(neutron.feed_forward(values))
+		print(str(layer_result))
+		layer_results.append(layer_result)
+		
+		for index in range(1, len(self.neutron_layers)):
+			prev_layer_result = layer_result
+			layer_result = []
+			for neutron in self.neutron_layers[index]:
+				layer_result.append(neutron.feed_forward(prev_layer_result))
+			print(str(layer_result))
+			layer_results.append(layer_result)			
 
 if __name__ == "__main__":
 	print("Main called")
-	perceptron = Perceptron(4)
-	
-	for i in range(0, 100):
-		print("Iteration: " + str(i) + ": " + str(perceptron.learn([1, 0, 0, 1], 1)))
+	network = Network(1, 4, 1)
+	network.feed_forward([1, 1, 1, 1])	
+
+	#neutron = Neutron(4)	
+	#for i in range(0, 1000):
+	#	print("Iteration: " + str(i) + ": " + str(neutron.learn([1, 1, 0, 1], 0.5)))
