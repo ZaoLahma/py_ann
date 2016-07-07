10b55be120ecddce80ae22b07b4fce6423c313dd
Janne Thyberg <thyberg.jan@gmail.com>
2016-07-07 08:53:45 +0000

    Added trainer and different activation function

--- simple_ann.py
+++ simple_ann.py
@@ -2,6 +2,7 @@
 
 from random import uniform
 from math import exp
+from math import tanh
 
 #------------------------------------------------
 def sigmoid(x):
@@ -9,21 +10,28 @@ def sigmoid(x):
 	
 def sigmoid_prim(x):
 	return (sigmoid(x) * (1 - x))
+
+def sigmoid_tanh(x):
+	return (tanh(x))
+	
+def sigmoid_tanh_prim(x):
+	return (1.0 - x**2)
 #------------------------------------------------
 
 def make_matrix(X, Y, val=0.0):
 	ret_val = []
 	for x in range(X):
 		ret_val.append([val] * Y)
-		
-	print("Created matrix: " + str(ret_val))
 	return ret_val
 	
 class NeuralNet:
-	def __init__(self, num_in, num_hid, num_out):
+	def __init__(self, num_in, num_hid, num_out, sigmoid_func, sigmoid_func_prim):
 		self.num_in = num_in + 1
 		self.num_hid = num_hid
 		self.num_out = num_out
+		
+		self.sigmoid = sigmoid_func
+		self.sigmoid_prim = sigmoid_func_prim
 		
 		self.activation_in = [1.0] * self.num_in
 		self.activation_hid = [1.0] * self.num_hid
@@ -38,8 +46,8 @@ class NeuralNet:
 				
 		for j in range(self.num_hid):
 			for k in range(self.num_out):
-				self.weights_out[j][k] = uniform(-1, 1)
-				
+				self.weights_out[j][k] = uniform(-1, 1)		
+								
 		self.change_in = make_matrix(self.num_in, self.num_hid)
 		self.change_out = make_matrix(self.num_hid, self.num_hid)
 		
@@ -52,23 +60,23 @@ class NeuralNet:
 			total = 0.0
 			for i in range(self.num_in):
 				total = total + self.activation_in[i] * self.weights_in[i][j]
-			self.activation_hid[j] = sigmoid(total)
+			self.activation_hid[j] = self.sigmoid(total)
 				
 		for k in range(self.num_out):
 			total = 0.0
 			for j in range(self.num_hid):
 				total = total + self.activation_hid[j] * self.weights_out[j][k]
-			self.activation_out[k] = sigmoid(total)
+			self.activation_out[k] = self.sigmoid(total)
 			
 		return self.activation_out
 
-	def back_prop(self, expected, N = 0.01, M  = 0.01):
+	def back_prop(self, expected, N, M):
 		
 		# Get ouput layer errors
 		output_errors = [0.0] * self.num_out
 		for k in range(self.num_out):
 			error = expected[k] - self.activation_out[k]
-			output_errors[k] = error * sigmoid_prim(self.activation_out[k])
+			output_errors[k] = error * self.sigmoid_prim(self.activation_out[k])
 			
 		# Get hidden layer errors	
 		hidden_errors = [0.0] * self.num_hid
@@ -76,7 +84,7 @@ class NeuralNet:
 			error = 0.0
 			for k in range(self.num_out):
 				error = error + output_errors[k] * self.weights_out[j][k]
-			hidden_errors[j] = error * sigmoid_prim(self.activation_hid[j])
+			hidden_errors[j] = error * self.sigmoid_prim(self.activation_hid[j])
 			
 		# Update output layer weights	
 		for j in range(self.num_hid):
@@ -97,12 +105,51 @@ class NeuralNet:
 		for k in range(len(expected)):
 			error += 0.5 * (expected[k] - self.activation_out[k])**2
 		return error
+
+class NeuralNetTrainer:
+	def __init__(self, net, acceptable_error = 0.01):
+		self.net = net
+		self.acceptable_error = acceptable_error
 		
-		
+	def train(self, patterns, expecteds, num_epochs = 10000, N = 0.1, M = 0.1):
+		for epoch in range(num_epochs):
+			training_complete = True
+			
+			for pattern_index in range(len(patterns)):
+				self.net.feed_forward(patterns[pattern_index])
+				error = self.net.back_prop(expecteds[pattern_index], N, M)
+				if error > self.acceptable_error:
+					training_complete = False
+					
+			if training_complete:
+				print("Acceptably low error (<" + str(self.acceptable_error) + ") achieved after " + str(epoch) + " epochs")
+				return
+				
+		print("Training completed without achieving acceptably low error (<" + str(self.acceptable_error) + ")")
 		
 if __name__ == "__main__":
-	net = NeuralNet(3, 5, 3)
+	patterns = [[0, 0], [0, 1], [1, 0], [1, 1]]
+	expected = [ [0],    [1],    [1],    [0]]
+	acceptable_error = 0.01
 	
-	for _ in range(10000):
-		print(net.feed_forward([1, 1, 1]))
-		print(net.back_prop([1, 0, 1]))
+	print("Training tanh network")
+	net_tanh = NeuralNet(2, 2, 1, sigmoid_tanh, sigmoid_tanh_prim)
+	trainer_tanh = NeuralNetTrainer(net_tanh, acceptable_error)
+	trainer_tanh.train(patterns, expected, 10000)
+	
+	#Test what the net should have learnt
+	print("Testing tanh network")
+	for pattern in patterns:
+		print(str(pattern) + " -> " + str(net_tanh.feed_forward(pattern)))
+		
+		
+	print("Training sigmoid network")	
+	net_sigmoid = NeuralNet(2, 8, 1, sigmoid, sigmoid_prim)
+	trainer_sigmoid = NeuralNetTrainer(net_sigmoid, acceptable_error)
+	trainer_sigmoid.train(patterns, expected, 10000)
+	
+	#Test what the net should have learnt
+	print("Testing sigmoid network")
+	for pattern in patterns:
+		print(str(pattern) + " -> " + str(net_sigmoid.feed_forward(pattern)))	
+	
